6f8c10d482e03ecc9c9ef4021b81f953d0a0dee2
Mira Tumanova <mira0tumanova@gmail.com>
2020-06-08 15:08:14 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component'; //!
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent //!
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
